aa539f200b6d9575083ea2b95291f7b430cc83e6
PCG06 <pcggaming06@gmail.com>
2024-03-27 03:55:47 +0530

    Show dynamic type for new move

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -3939,7 +3939,7 @@ static void SetMonTypeIcons(void)
 
 static void SetMoveTypeIcons(void)
 {
-    u8 i = 0;
+    u8 i = 0, movetype;
     struct PokeSummary *summary = &sMonSummaryScreen->summary;
     struct Pokemon *mon = &sMonSummaryScreen->currentMon;
 
@@ -3949,7 +3949,7 @@ static void SetMoveTypeIcons(void)
         {
             if (B_DYNAMIC_MOVE_TYPE == TRUE)
             {
-                u8 movetype = GetMonMoveType(summary->moves[i], mon, 0);
+                movetype = GetMonMoveType(summary->moves[i], mon, 0);
                 SetTypeSpritePosAndPal(movetype, 85, 32 + (i * 16), i + SPRITE_ARR_ID_TYPE);
             }
             else
@@ -3975,6 +3975,9 @@ static void SetContestMoveTypeIcons(void)
 
 static void SetNewMoveTypeIcon(void)
 {
+    u8 moveType;
+    struct Pokemon *mon = &sMonSummaryScreen->currentMon;
+
     if (sMonSummaryScreen->newMove == MOVE_NONE)
     {
         SetSpriteInvisibility(SPRITE_ARR_ID_TYPE + 4, TRUE);
@@ -3982,7 +3985,14 @@ static void SetNewMoveTypeIcon(void)
     else
     {
         if (sMonSummaryScreen->currPageIndex == PSS_PAGE_BATTLE_MOVES)
-            SetTypeSpritePosAndPal(gMovesInfo[sMonSummaryScreen->newMove].type, 85, 96, SPRITE_ARR_ID_TYPE + 4);
+                
+            if (B_DYNAMIC_MOVE_TYPE == TRUE)
+            {
+                moveType = GetMonMoveType(sMonSummaryScreen->newMove, mon, 0);
+                SetTypeSpritePosAndPal(moveType, 85, 96, SPRITE_ARR_ID_TYPE + 4);
+            }
+            else
+                SetTypeSpritePosAndPal(gMovesInfo[sMonSummaryScreen->newMove].type, 85, 96, SPRITE_ARR_ID_TYPE + 4);
         else
             SetTypeSpritePosAndPal(NUMBER_OF_MON_TYPES + gMovesInfo[sMonSummaryScreen->newMove].contestCategory, 85, 96, SPRITE_ARR_ID_TYPE + 4);
     }
